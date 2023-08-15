c4073c6fdee48a210a14987495e3332241813d2b
Julian Uy <uyjulian@gmail.com>
2023-08-15 16:07:43 +0000

    Use win32 lean and mean

--- CharacterSet.h
+++ CharacterSet.h
@@ -2,7 +2,11 @@
 #ifndef __CharacterSet_H__
 #define __CharacterSet_H__
 
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
 #include <windows.h>
+#include <objidl.h>
 #include "tp_stub.h"
 
 #define tjs_string std::wstring
